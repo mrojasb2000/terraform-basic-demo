07d0f43f1202bea3dbc138e1a49bcd9dbaadf9e0
Mauricio Rojas <mrojasb2000@gmail.com>
2020-07-02 13:05:11 -0400

    [@Mavro] Add provider, basic infrastructure AWS.

---
+++ main.tf
@@ -0,0 +1,65 @@
+provider "aws" {
+    region = "${var.region}"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.key_pair.key_name}"
+  security_groups = ["${aws_security_group.lemontech_sg.name}"]
+
+  tags = {
+    Name = "Lemontch"
+  }
+}
+
+resource "aws_security_group" "lemontech_sg" {
+  name = "lemontech_sg"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    # To keep this example simple, we allow incoming SSH requests from any IP. In real-world usage, you should only
+    # allow SSH requests from trusted servers, such as a bastion host or VPN server.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    # To keep this example simple, we allow incoming SSH requests from any IP. In real-world usage, you should only
+    # allow SSH requests from trusted servers, such as a bastion host or VPN server.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow outgoing traffic to anywhere.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name = "lemontech-key"
+  public_key = "${file("./keys/aws-terraform.pub")}" 
+}
