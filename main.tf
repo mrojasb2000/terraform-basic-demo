59400c37043b27409629a1504ab605942dd1ccfa
Mauricio Rojas <mrojasb2000@gmail.com>
2020-07-02 20:17:39 -0400

    [@Mavro] Add gateway, route table and route table association.

--- main.tf
+++ main.tf
@@ -15,13 +15,42 @@ resource "aws_subnet" "lemontech_subnet" {
   vpc_id     = aws_vpc.lemontech_vpc.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-2a"
+  //availability_zone = "us-east-2a"
 
   tags = {
     Name = "Lemontech Subnet"
   }
 }
 
+resource "aws_internet_gateway" "lemontech_gateway" {
+  vpc_id = aws_vpc.lemontech_vpc.id
+
+  tags = {
+    Name = "Lemontech Gateway"
+  }
+}
+
+resource "aws_route_table" "lemontech_route_table" {
+  vpc_id = aws_vpc.lemontech_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.lemontech_gateway.id
+  }
+
+  tags = {
+    Name = "lemontech Route Table"
+  }
+}
+
+resource "aws_route_table_association" "lemontech_route_table_association" {
+  subnet_id      = aws_subnet.lemontech_subnet.id
+  route_table_id = aws_route_table.lemontech_route_table.id
+
+  tags = {
+    Name = "Lemontech Route Table Association"
+  }
+}
 
 /* data "aws_ami" "ubuntu" {
   most_recent = true
