2b502291510b1e0eb5ab8a5a06e903261d770640
Mauricio Rojas <mrojasb2000@gmail.com>
2020-07-02 20:43:35 -0400

    [@Mavro] Add two subnet and two route table association.

--- main.tf
+++ main.tf
@@ -11,14 +11,36 @@ resource "aws_vpc" "lemontech_vpc" {
   }
 }
 
-resource "aws_subnet" "lemontech_subnet" {
+resource "aws_subnet" "lemontech_subnet_1" {
   vpc_id     = aws_vpc.lemontech_vpc.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  //availability_zone = "us-east-2a"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "Lemontech Subnet"
+    Name = "Lemontech Subnet 1"
+  }
+}
+
+resource "aws_subnet" "lemontech_subnet_2" {
+  vpc_id     = aws_vpc.lemontech_vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Lemontech Subnet 2"
+  }
+}
+
+resource "aws_subnet" "lemontech_subnet_3" {
+  vpc_id     = aws_vpc.lemontech_vpc.id
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "Lemontech Subnet 3"
   }
 }
 
@@ -43,11 +65,20 @@ resource "aws_route_table" "lemontech_route_table" {
   }
 }
 
-resource "aws_route_table_association" "lemontech_route_table_association" {
-  subnet_id      = aws_subnet.lemontech_subnet.id
+resource "aws_route_table_association" "lemontech_route_table_association_1" {
+  subnet_id      = aws_subnet.lemontech_subnet_1.id
   route_table_id = aws_route_table.lemontech_route_table.id
 }
 
+resource "aws_route_table_association" "lemontech_route_table_association_2" {
+  subnet_id      = aws_subnet.lemontech_subnet_2.id
+  route_table_id = aws_route_table.lemontech_route_table.id
+}
+
+resource "aws_route_table_association" "lemontech_route_table_association_3" {
+  subnet_id      = aws_subnet.lemontech_subnet_3.id
+  route_table_id = aws_route_table.lemontech_route_table.id
+}
 /* data "aws_ami" "ubuntu" {
   most_recent = true
 
