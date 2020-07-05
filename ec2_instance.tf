97fe8cc56d257910bf24f498fe1424b05c1593f7
Mauricio Rojas <mrojasb2000@gmail.com>
2020-07-04 20:16:58 -0400

    [@Mavro] Add private ip address to EC2 instance.

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -4,9 +4,11 @@ resource "aws_instance" "lemontech_server_1" {
     associate_public_ip_address = true
     subnet_id = aws_subnet.lemontech_subnet_1.id
     vpc_security_group_ids = ["${aws_security_group.lemontech_security_group_1.id}", "${aws_security_group.lemontech_security_group_2.id}"]
+    private_ip = "10.0.1.50"
 
     tags = {
       "Name" = "Lemontech Instance 1"
+      Owner = "Lemontech"
     }
 }
 
