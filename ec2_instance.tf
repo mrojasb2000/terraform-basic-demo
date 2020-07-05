e628fecec6a7cf9de1ce6bf1d4d21ae284e93fae
Mauricio Rojas <mrojasb2000@gmail.com>
2020-07-04 20:51:33 -0400

    [@Mavro] Refactor private ip address to EC2 instance.

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "lemontech_server_1" {
     subnet_id = aws_subnet.lemontech_subnet_1.id
     vpc_security_group_ids = ["${aws_security_group.lemontech_security_group_1.id}", "${aws_security_group.lemontech_security_group_2.id}"]
     private_ip = "10.0.1.50"
+    key_name = aws_key_pair.lemontech_key_pair.id
 
     tags = {
       "Name" = "Lemontech Instance 1"
